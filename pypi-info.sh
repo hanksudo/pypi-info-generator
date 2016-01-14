3d5e9403ad6052c9b5f8dcc0c7d3a57ee7b41f02
Hank Wang <drapho@gmail.com>
2016-01-14 22:59:55 +0800

    ignore blank line in file

--- pypi-info.sh
+++ pypi-info.sh
@@ -12,7 +12,7 @@ then
   usage
 fi
 
-grep -v '^#' $1 | while read -r line ; do
+grep -Ev '^$|#' $1 | while read -r line ; do
     package=`echo $line | awk -F'==' '{print $1}'`
     curl -L https://pypi.python.org/pypi/$package/json 2>/dev/null | jq -r '.info | .summary + "\n" + .home_page' | sed 's/^/# /' | tee -a new-$1
     echo $line | tee -a new-$1
